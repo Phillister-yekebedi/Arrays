e4acd0dbe83cc9bd5a10746f87491b40a2dc4625
phillister-yekebedi <phillisteryekebedi@gmail.com>
2023-03-17 19:28:13 +0300

    first commit

---
+++ Arrays.js
@@ -0,0 +1,43 @@
+// 1.Find the last element of the following arrays.
+//a)arr1 = [3,7,34,90,12]
+let arr1= [3,7,34,90,12];
+g = (arr1.length-1);
+console.log("last element is"+g);
+
+
+//b)arr2 = [true, "green", "where",12,56]
+let arr2 = [true, "green", "where",12,56];
+v= arr2[arr2.length-1];
+console.log("last element is"+ v);
+
+
+//2.Write a JS program that will join the following array elements into a string
+//myPets = ["Cow", "Python", "Snake", "Dog"];
+let myPets = ["Cow", "Python", "Snake", "Dog"];
+console.log(myPets.join());
+
+
+//3.Write a JS script to sort the following array items
+//var arr3 = [ -5, 9, 5, 3, 2, -3, 6, 8, 4, 1 ]
+var arr3 = [ -5, 9, 5, 3, 2, -3, 6, 8, 4, 1 ];
+console.log(arr3.sort());
+
+//4.Write a program to remove duplicates from the following array.
+//Console the array without duplicates, and console another array that only contains the duplicates
+//var arr = ["apple", "mango", "apple", "orange", "mango", "mango"];
+var arr = ["apple", "mango", "apple", "orange", "mango", "mango"];
+// console.log [...new (set(arr))];
+
+//5.Write a JS script to search for the following word in the array.
+4
+//If the word is present, console it else console "the search word was not found"
+//let arr5 = ["the", "way", "x", 4, 23];
+let arr5 = ["the", "way", "x", 4, 23];
+
+
+
+//6.Write a JS script to sort the following string
+//let word = "lufituaeb"
+let word = "lufituaeb"
+console.log(word.sort());
+
